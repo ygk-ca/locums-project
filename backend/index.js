85367f398562e72a5d791912951c1b10bfb23434
Hashir <50779880+Hashir103@users.noreply.github.com>
2023-06-24 21:37:46 -0400

    add frontend info back

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,5 @@
 'use strict';
 const express = require('express');
-const cors = require('cors');
 const bodyParser = require('body-parser');
 const config = require('./config');
 const locum_routes = require('./routes/locum_routes');
@@ -8,7 +7,6 @@ const locum_routes = require('./routes/locum_routes');
 const app = express();
 
 app.use(express.json());
-app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/api', locum_routes.routes);
