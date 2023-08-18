b95a209ca0b2af60ef82ca131ef1744cbbd29596
Hashir <50779880+Hashir103@users.noreply.github.com>
2023-08-18 18:42:46 -0400

    add dynamic shift listing

--- frontend/src/app/component/clinic/clinic.component.ts
+++ frontend/src/app/component/clinic/clinic.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { AuthService } from 'src/app/shared/auth.service';
 import { UserService } from '../users/services/user.service';
 import { User } from '../users/models/user';
-import { Observable, filter, switchMap } from 'rxjs';
+import { Observable, filter, switchMap, firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-clinic',
@@ -13,17 +14,28 @@ import { Observable, filter, switchMap } from 'rxjs';
 export class ClinicComponent {
 
   user$: Observable<User>;
+  clinicShifts: any;
 
-  constructor(private auth: AuthService, private afAuth: AngularFireAuth, private userService: UserService) {}
+  constructor(private auth: AuthService, private afAuth: AngularFireAuth, private userService: UserService, private afs: AngularFirestore) {}
 
-  ngOnInit() {
+  async ngOnInit() {
     this.user$ = this.afAuth.user.pipe(
       filter(user => !!user),
       switchMap(user => this.userService.user$(user!.uid))
     );
+    this.clinicShifts = await this.getShifts();
   }
 
   logout() {
     this.auth.logout()
+  }
+
+  async getShifts() {
+    const email = await firstValueFrom(this.afAuth.user).then(user => user?.email || "err");
+    const clinicShifts = await firstValueFrom(this.afs.collection("clinics").doc(email).get()).then(doc => doc.data()!["shifts"]);
+    if (Object.keys(clinicShifts).length == 0) {
+      return ['No Shifts Submitted']
+    }
+    return clinicShifts;
   }
 }
